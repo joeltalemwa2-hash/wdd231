eebe447ceff9ec71f7a42ae8a80272da6097334f
joeltalemwa2-hash <jtalemwa@byupathway.edu>
2026-05-28 18:11:19 +0300

    jemo

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const spotlightURL =
 "data/members.json";
 
@@ -35,6 +34,11 @@ function displaySpotlights(members) {
 
         card.classList.add("spotlight-card");
 
+        const level =
+        member.membership === 3
+        ? "Gold"
+        : "Silver";
+
         card.innerHTML = `
         <h3>${member.name}</h3>
 
@@ -42,6 +46,7 @@ function displaySpotlights(members) {
 
         <img src="images/${member.image}"
              alt="${member.name}"
+             loading="lazy"
              width="100"
              height="100">
 
@@ -61,76 +66,9 @@ function displaySpotlights(members) {
         </div>
         `;
 
-        container.appendChild(card);
-    });
-}
-
-=======
-const spotlightURL =
-"data/members.json";
-
-const spotlightContainer =
-document.querySelector("#spotlights");
-
-async function getSpotlights() {
-
-    const response =
-    await fetch(spotlightURL);
-
-    const data =
-    await response.json();
-
-    displaySpotlights(data);
-}
-
-function displaySpotlights(members) {
-
-    const premiumMembers =
-    members.filter(member =>
-    member.membership >= 2);
-
-    const shuffled =
-    premiumMembers.sort(() => 0.5 - Math.random());
-
-    const selected =
-    shuffled.slice(0, 3);
-
-    selected.forEach(member => {
-
-        const card =
-        document.createElement("div");
-
-        card.classList.add("spotlight-card");
-
-        const level =
-        member.membership === 3
-        ? "Gold"
-        : "Silver";
-
-        card.innerHTML = `
-        <img src="images/${member.image}"
-             alt="${member.name}"
-             loading="lazy"
-             width="100"
-             height="100">
-
-        <h3>${member.name}</h3>
-
-        <p>${member.phone}</p>
-
-        <p>${member.address}</p>
-
-        <p>${level} Member</p>
-
-        <a href="${member.website}"
-           target="_blank">
-           Visit Website
-        </a>
-        `;
-
         spotlightContainer.appendChild(card);
     });
 }
 
->>>>>>> 9814f035b781fe729519086e1ac09306a338dbc3
 getSpotlights();
+
