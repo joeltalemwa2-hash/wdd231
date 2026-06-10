4d91c97d7a70861ed644aef73eafa45f4a65f774
joeltalemwa2-hash <jtalemwa@byupathway.edu>
2026-06-10 17:07:03 +0300

    coll

--- chamber/data/places.mjs
+++ chamber/data/places.mjs
@@ -1,31 +1,31 @@
 export const places = [
 {
     name: "Kasubi Tombs",
-    image: "images/kasubi.webp",
+    image: "images/ganda.jpg",
     address: "Kasubi Hill, Kampala",
     description: "UNESCO World Heritage site and burial ground of Buganda kings."
 },
 {
     name: "Uganda Museum",
-    image: "images/uganda-museum.webp",
+    image: "images/uganda-museum.jpg",
     address: "Kitante Road, Kampala",
     description: "Uganda's largest museum preserving cultural and historical artifacts."
 },
 {
     name: "Baháʼí Temple",
-    image: "images/bahai.webp",
+    image: "images/aa.jpg",
     address: "Kikaya Hill, Kampala",
     description: "The only Baháʼí House of Worship in Africa."
 },
 {
     name: "Gaddafi National Mosque",
-    image: "images/gaddafi.webp",
+    image: "images/gadafi-mosque.webp",
     address: "Old Kampala",
     description: "One of the largest mosques in East Africa."
 },
 {
     name: "Ndere Cultural Centre",
-    image: "images/ndere.webp",
+    image: "images/bb.jpg",
     address: "Ntinda, Kampala",
     description: "Showcases traditional Ugandan music and dance."
 },
@@ -37,13 +37,13 @@ export const places = [
 },
 {
     name: "Kabaka's Palace",
-    image: "images/lubiri.webp",
+    image: "images/k.jpg",
     address: "Mengo Hill, Kampala",
     description: "Official palace of the King of Buganda."
 },
 {
     name: "Independence Monument",
-    image: "images/independence.webp",
+    image: "images/last.webp",
     address: "Speke Road, Kampala",
     description: "Symbol of Uganda's independence."
 }
